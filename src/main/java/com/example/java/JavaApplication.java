06767cfc1be8e3ca698efce07943f52a33d5714d
hoseacodes <dominique11h@yahoo.com>
2021-03-19 17:23:01 -0500

    Refactored Java App

--- src/main/java/com/example/java/JavaApplication.java
+++ src/main/java/com/example/java/JavaApplication.java
@@ -19,23 +19,12 @@ public class JavaApplication {
 		return String.format("Hello %s!", name);
 	}
 
-	public class Greeting {
-
-		private final long id;
-		private final String content;
-	
-		public Greeting(long id, String content) {
-			this.id = id;
-			this.content = content;
-		}
-	
-		public long getId() {
-			return id;
-		}
-	
-		public String getContent() {
-			return content;
-		}
-	}
 
 }
+
+
+		
+
+		
+
+		
