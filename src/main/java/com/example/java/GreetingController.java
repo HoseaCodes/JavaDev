40cba0dfc202485fe08aadfb7855099292c9ad13
hosead6168 <dominique11h@yahoo.com>
2021-07-27 16:36:53 -0500

    Added controller comments

--- src/main/java/com/example/java/GreetingController.java
+++ src/main/java/com/example/java/GreetingController.java
@@ -6,13 +6,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-//shorthand for @Controller and RepsonseBody
+// Shorthand for @Controller and RepsonseBody
+// Marks as a controller where every method returns a
+// domain object instead of a view
 @RestController
 public class GreetingController {
 
 	private static final String template = "Hello, %s!";
 	private final AtomicLong counter = new AtomicLong();
 
+	// Ensures that HTTP GET requests to /greeting
+	// are mapped to the greeting() method
 	@GetMapping("/greeting")
 	// RequestParam binds the value of the query string parameter name into the name
 	// paramater of the greeting method
