b88103b60c18ab47db258989b2ad2314b9f5cf22
hosead6168 <dominique11h@yahoo.com>
2021-07-27 17:39:30 -0500

    Added home endpoint testing

---
+++ src/test/java/com/example/java/HelloControllerTest.java
@@ -0,0 +1,32 @@
+package com.example.java;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import jdk.jfr.Timestamp;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class HelloControllerTest {
+
+    @Autowired 
+    // MockMvc comes from Spring Testand lets you set builder classes
+    // to send HTTP requests into the DispatcherServlet and make assertions
+    private MockMvc mvc;
+
+    @Timestamppublic void getHello() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
+                                        .andExpect(status().isOk())
+                                        .andExpect(content().string(equalTo("Greetings from Spring Boot!")));
+    }
+}
