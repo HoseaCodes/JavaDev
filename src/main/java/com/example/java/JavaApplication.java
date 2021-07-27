93855f3eda61631efca3cd3835a84731a60b10f0
hosead6168 <dominique11h@yahoo.com>
2021-07-27 17:27:02 -0500

    Added home index endpoint

--- src/main/java/com/example/java/JavaApplication.java
+++ src/main/java/com/example/java/JavaApplication.java
@@ -1,7 +1,11 @@
 package com.example.java;
 
+import java.util.Arrays;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,7 +13,6 @@ import org.springframework.web.bind.annotation.RestController;
 // REST API
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
@@ -33,6 +36,19 @@ public class JavaApplication {
 	//uses the Jackson JSON processing library to process the incoming data.
 	public RestTemplate restTemplate(RestTemplateBuilder builder) {
 		return builder.build();
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+		return args -> {
+			System.out.println("Let's inspect the beans provided by Spring Boot:");
+			
+			String[] beanNames = ctx.getBeanDefinitionNames();
+			Arrays.sort(beanNames);
+			for (String beanName :beanNames) {
+				System.out.println(beanName);
+			}
+		};
 	}
 
 	@Bean
