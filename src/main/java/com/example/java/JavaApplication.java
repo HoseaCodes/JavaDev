265124c8be780997bd0c8818d7b967adae38440e
hosead6168 <dominique11h@yahoo.com>
2021-07-28 05:35:10 -0500

    Added OAuth Login for GitHub

--- src/main/java/com/example/java/JavaApplication.java
+++ src/main/java/com/example/java/JavaApplication.java
@@ -1,12 +1,22 @@
 package com.example.java;
 
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Map;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,14 +29,38 @@ import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @RestController
-public class JavaApplication {
+public class JavaApplication extends WebSecurityConfigurerAdapter {
 
 	private static final Logger log = LoggerFactory.getLogger(JavaApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(JavaApplication.class, args);
 	}
-
+	@GetMapping("/user")
+    public Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal) {
+        return Collections.singletonMap("name", principal.getAttribute("name"));
+    }
+	@Override
+    protected void configure(HttpSecurity http) throws Exception {
+    	// @formatter:off
+        http
+            .authorizeRequests(a -> a
+                .antMatchers("/", "/error", "/webjars/**").permitAll()
+                .anyRequest().authenticated()
+            )
+			.csrf(c -> c
+            .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+        	)
+            .exceptionHandling(e -> e
+                .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+            )
+			.logout(l -> l
+            .logoutSuccessUrl("/").permitAll()
+        	)
+            .oauth2Login();
+			
+        // @formatter:on
+    }
 	@GetMapping("/hello")
 	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return String.format("Hello %s!", name);
